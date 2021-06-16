aa2ae819f9e807df7cf41ee9633770a5e0baa231
ltetrel <loic.tetrel.pro@gmail.com>
2021-06-16 17:35:50 -0400

    adding some ideas for visualization

---
+++ src/visualization/visualize.py
@@ -0,0 +1,8 @@
+"""
+We could also provide some visualization tools.
+From basic graph plotting using https://networkx.org/documentation/stable/tutorial.html
+Eigen vector representation: https://persagen.com/files/misc/arxiv-1712.00468.png
+Basic clustering: https://github.com/neurolibre/gcn_tutorial_test
+GCN latent space representation (T-SNE or PCA)
+Graph signal reconstruction (for graph-AE)
+"""
