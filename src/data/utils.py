2f10689c5bca38b6fa1d4da15129ddffc88ee676
ltetrel <loic.tetrel.pro@gmail.com>
2021-07-09 18:32:15 -0400

    numpy naming

--- src/data/utils.py
+++ src/data/utils.py
@@ -1,13 +1,12 @@
-import numpy.lib.format
 import numpy as np
 from sklearn.model_selection import train_test_split
 
 # https://stackoverflow.com/questions/64226337/is-there-a-way-to-read-npy-header-without-loading-the-whole-file
 def read_npy_array_header(filepath):
-    with open(filepath, 'rb') as fobj:
-      version = numpy.lib.format.read_magic(fobj)
+    with open(filepath, "rb") as fobj:
+      version = np.lib.format.read_magic(fobj)
       func_name = 'read_array_header_' + '_'.join(str(v) for v in version)
-      func = getattr(numpy.lib.format, func_name)
+      func = getattr(np.lib.format, func_name)
       header = func(fobj)
     
     return header
