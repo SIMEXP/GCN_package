761b466fcd3303ec9f9b653b9d66213956144f07
ltetrel <loic.tetrel.pro@gmail.com>
2021-09-24 12:45:36 -0400

    autoencore info

--- simexp_gcn/data/raw_data_loader_bids.py
+++ simexp_gcn/data/raw_data_loader_bids.py
@@ -1,3 +1,7 @@
+import os
+import utils
+import bids
+
 #TODO: BIDS detection
   #1 first detect if input data is BIDS, and was preprocessed with fmriprep
     # a. if BIDS but not fmriprep: ask user to preprocess OR povide folder with timeseries + connectomes + events (optionnally)
@@ -10,4 +14,20 @@
 #TODO: prepare data and labels from --prediction or --autoencoding
   # 1. let user choose whether it will perfom a prediction or encoding
     # a. if --prediction: read participants.tsv to get prediction target (user choice for which column to use, for example "sex", "group" or	"age")
-    # b. if --autoencoder: skip  
+    # b. if --autoencoder: skip
+
+class RawBidsLoader():
+
+  def __init__(self, input_dir, atlas_path=None, atlas_parcels=512):
+    self.input_dir = input_dir
+    self.atlas_path = atlas_path
+    self.atlas_parcels = atlas_parcels
+
+    if self.atlas_path is None:
+      difumo_dir = os.path.join(os.path.dirname(__file__), "..", "..", "data", "raw")
+      utils.fetch_difumo(difumo_dir, n_parcels=self.atlas_parcels)
+
+    
+
+  
+  
